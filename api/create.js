f07809e37841e9e2908e2ceb7352512f6492cacf
vijayendrasingh1 <vijayendrasingh926@gmail.com>
2026-04-02 22:41:28 +0530

    Update create.js

--- api/create.js
+++ api/create.js
@@ -1,22 +1,17 @@
-import fs from "fs";
+let links = {};
 
 export default function handler(req, res) {
 
   const { folder, url } = req.query;
 
-  const file = "./data/links.json";
-  let data = JSON.parse(fs.readFileSync(file));
-
   const id = Math.random().toString(36).substring(2, 8);
 
-  if (!data[folder]) data[folder] = {};
+  if (!links[folder]) links[folder] = {};
 
-  data[folder][id] = {
+  links[folder][id] = {
     url: url,
     clicks: 0
   };
-
-  fs.writeFileSync(file, JSON.stringify(data, null, 2));
 
   res.send({
     shortLink: `${req.headers.host}/${folder}/${id}`
