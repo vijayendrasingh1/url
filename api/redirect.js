bd9da92afe3359729ce2444416bbcfeab97d3d1e
vijayendrasingh1 <vijayendrasingh926@gmail.com>
2026-04-02 22:46:44 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,25 +1,20 @@
-import fs from "fs";
+let links = {
+  rtry: {
+    wxo9ez: "https://google.com"
+  }
+};
 
 export default function handler(req, res) {
-
-  const file = "./data/links.json";
-  const data = JSON.parse(fs.readFileSync(file));
 
   const parts = req.url.split("/");
   const folder = parts[1];
   const id = parts[2];
 
-  if (data[folder] && data[folder][id]) {
-
-    data[folder][id].clicks++;
-
-    fs.writeFileSync(file, JSON.stringify(data, null, 2));
-
+  if (links[folder] && links[folder][id]) {
     res.writeHead(302, {
-      Location: data[folder][id].url
+      Location: links[folder][id]
     });
     res.end();
-
   } else {
     res.status(404).send("Invalid Link");
   }
